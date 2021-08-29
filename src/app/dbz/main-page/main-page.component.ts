ea63879c7261d23abdd43543d5b460f05e6e6de0
Jorge Ramos <jose.ramos@nextiva.com>
2021-08-29 15:00:19 -0500

    Integrate form to add characters

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Character {
+  name: string;
+  power: string;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
+  characters: Character[] = [
+    {
+      name: 'Goku',
+      power: '15000',
+    },
+    {
+      name: 'Vegetta',
+      power: '10000',
+    },
+  ];
 
-  constructor() { }
+  newCharacter: Character = {
+    name: '',
+    power: '',
+  };
 
-  ngOnInit(): void {
+  addCharacter() {
+    if (this.newCharacter.name.trim().length === 0) {
+      return;
+    }
+    this.characters.push(this.newCharacter);
+    this.newCharacter = {
+      name: '',
+      power: '',
+    };
   }
-
 }
