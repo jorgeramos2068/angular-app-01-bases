84fe138a4ebfa37937004cbfcba8a0d71ab1f0bf
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:08:50 -0600

    Integrate form to add characters

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [MainPageComponent],
   exports: [MainPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class DbzModule {}
