ccb18edb768bb3e7b73767aa2ffaeb799f3fb5fd
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:08:48 -0600

    Add counter in App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Bases App';
+  public counter: number = 0;
+  public base: number = 5;
+
+  accumulate(value: number): void {
+    this.counter += value;
+  }
 }
